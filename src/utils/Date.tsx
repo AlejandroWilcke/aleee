025da18768eeddd91b78d37fa35d613732a243ff
Ale <aleee9208@hotmail.com>
2021-04-14 00:00:40 -0300

    Date was returning a bad month

--- src/utils/Date.tsx
+++ src/utils/Date.tsx
@@ -1,4 +1,4 @@
 export function parseDate( plainDate: string ){
     let d = new Date(plainDate);
-    return `${d.getFullYear()}/${d.getMonth()}/${d.getDate()}, ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}, ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
 }
